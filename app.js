15f34898566e8666bf29719f3e6665b930d0ad81
rjames187 <rory.james2021@gmail.com>
2022-05-01 21:31:54 -0500

    add access control allow origin header

--- app.js
+++ app.js
@@ -6,6 +6,16 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
+// Add Access Control Allow Origin headers
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.post("/infix", (req, res) => {
   res.json(tools.infix(req.body.data));
 });
